ecf095a8fc93d48119c2eacbb166044f8fca0fe6
Daniele Scasciafratte <mte90net@gmail.com>
2019-10-21 12:43:44 +0200

    finished new parameter

--- word_usage.py
+++ word_usage.py
@@ -10,6 +10,7 @@ min_frequency = 0
 max_frequency = 0
 words_only = False
 no_repeats = False
+split_apostrophe = False
 
 
 def clean(line, split=False):
@@ -60,6 +61,8 @@ for opt, arg in opts:
         min_frequency = int(arg)
     elif opt == "--max-frequency":
         max_frequency = int(arg)
+    elif opt == "--strip-by-apostrophe":
+        split_apostrophe = True
     elif opt == "--show-words-only":
         words_only = True
     elif opt == "--no-repeats":
@@ -74,7 +77,7 @@ if not os.path.exists(input_file):
 # Scan sentences
 with open(input_file) as f:
     for line in f:
-        words = clean(line, True)
+        words = clean(line, split_apostrophe)
         repeat_list = []
 
         for w in words:
